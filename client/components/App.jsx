a08285ffb23eb62c3497641de86b200ebd1541f3
don-harris <donald.walter.harris@gmail.com>
2018-01-08 12:33:20 +1300

    fist route set up

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import {HashRouter as Router, Route} from 'react-router-dom'
-import Greetings from './Greetings'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+
+import Signup from './Signup' 
 
 const App = () => (
   <Router>
     <div className='app-container'>
       <h1 className="title is-1">Mana Archery Club</h1>
-      <Route exact path="/" component={Greetings} />
+      <Route path='/signup' component={Signup}/>
       <div>
-        <button>Sign Up</button>
+        <button><Link to='/signup'>Sign Up</Link></button>
         </div>
     </div>
   </Router>
